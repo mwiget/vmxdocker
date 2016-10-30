da3d070092e4db14db07e23d464bb85b1b653d07
Iddo Cohen <icohen@juniper.net>
2016-10-30 09:38:03 +0100

    Fixed and added new features with 16.1 release
    Bugs:
    
    - Fixed: path checking for VCP Image - As for 16.1 junos*qcow2 is used and not jinstall64*img.
    - Fixed: handling of ls aborting - Now checking only empty string; without, script aborting at ls exit code.
    
    Features:
    
    - Added VCPMEM for handling vRE MEM - From 16.1, 1GB can be used (no fix 2GB needed). Default will still be 2GB
    - Edited README for first feature.
    - Edited run-vmx1.sh for first feature.

--- run-vmx1.sh
+++ run-vmx1.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-docker run --name vmx1 --rm --privileged --net=host \
+sudo docker run --name vmx1 --rm --privileged --net=host \
   -v $PWD:/u:ro \
-  --env TAR="vmx-15.1F3.11.tgz" \
+  --env TAR="vmx-bundle-16.1R2.11.tgz" \
   --env CFG="vmx1.cfg" \
-  --env CONFIG="vmx1.conf.txt" \
   --env DEV="br0 br0" \
-  --env MEM="8000" --env VCPU="7" \
+  --env VCPMEM="1000" \
+  --env MEM="4000" \
+  --env VCPU="3" \
   -i -t marcelwiget/vmx:latest
